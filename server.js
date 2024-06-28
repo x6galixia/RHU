d96d8bcc12b65a6e97b69afe2b523f5416c4496a
x6galixia <147425442+x6galixia@users.noreply.github.com>
2024-06-29 01:17:44 +0800

    added a routes and view

--- server.js
+++ server.js
@@ -2,14 +2,28 @@ const express = require('express')
 const app = express()
 const expressLayout = require('express-ejs-layouts')
 
+//router
 const indexRouter = require('./routes/index')
+const loginRouter = require('./routes/login')
+const medtechRouter = require('./routes/medtech')
+const nurseRouter = require('./routes/nurse')
+const doctorRouter = require('./routes/doctor')
+const pharmacistRouter = require('./routes/pharmacist')
 
+//initialize layout
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayout)
 app.use(express.static('public'))
 
+//this is all of the routes
 app.use('/', indexRouter)
+app.use('/login', loginRouter)
+app.use('/medtech', medtechRouter)
+app.use('/nurse', nurseRouter)
+app.use('/doctor', doctorRouter)
+app.use('/pharmacist', pharmacistRouter)
 
+//port
 app.listen(process.env.PORT || 3000)
